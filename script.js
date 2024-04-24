c878407f51000338f7ab48b06ed502707a3a683c
Lum <121110537+LumShishko@users.noreply.github.com>
2024-04-24 16:47:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function Student(name, surname, dateofbirth, id, level){
+    this.name = name;
+    this.surname = surname;
+    this.dateofbirth = dateofbirth;
+    this.id = id;
+    this.level = level;
+
+};
+
+Student.prototype.displayDetails = function(){
+    console.log("Name: " + this.name)
+    console.log("Surname: " + this.surname)
+    console.log("Date of birth: " + this.dateofbirth)
+    console.log("Id: " + this.id)
+    console.log("Level: " + this.level)
+}
+
+
+function University() {
+    this.students = [];
+}
+
+University.prototype.addStudent = function(student) {
+    this.students.push(student);
+}
+
+
+
+
+
+University.prototype.displayStudents = function() {
+    console.log("Studentet ne universitetin tone jane: ");
+    for(var i = 0;i < this.Student.length; i++) {
+        console.log("Students:" + (i + 1) + " : ");
+        this.Student[i].displayDetails();
+    }
+}
+
+var university = new University();
+
+var student1 = new Student("George", "Orwell", 1994 , 21 , 32);
+var student2 = new Student("John", "Doe", 1999 , 42 , 42);
+var student3 = new Student("Joseph", "Parker", 1992 , 56 , 24);
+
+
+university.addStudent(student1);
+university.addStudent(student2);
+university.addStudent(student3);
+
+university.displayStudents();
